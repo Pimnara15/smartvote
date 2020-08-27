d80d4abdf918729059a43a5c689b639f77033a86
Pimpaparpron Junpect <piaszapim@gmail.com>
2020-08-28 00:57:57 +0700

    Initial commit

---
+++ app/src/main/java/th/ac/su/cp/smartvote/MainActivity.java
@@ -0,0 +1,16 @@
+package th.ac.su.cp.smartvote;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
